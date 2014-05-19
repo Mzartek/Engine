0f45a6a0a6f6a7115ba8b8b26d5314046601c619
mzartek <mzartek@hotmail.fr>
2014-05-20 00:27:59 +0200

    New Scene

--- demo/main.cpp
+++ demo/main.cpp
@@ -23,211 +23,211 @@ GLfloat sr, sg, sb, sa;
 
 void helicopterMatrixIdentity(void)
 {
-  helicopter.matIdentity();
-  grotor.matIdentity();
-  grotor.matTranslate(0.08f, 0.0f, -1.03f);
-  protor.matIdentity();
-  protor.matTranslate(0.25f, 2.40f, 19.75f);
+	helicopter.matIdentity();
+	grotor.matIdentity();
+	protor.matIdentity();
 }
 
 void helicopterMatrixScale(GLfloat x, GLfloat y, GLfloat z)
 {
-  helicopter.matScale(x, y, z);
-  grotor.matScale(x, y, z);
-  protor.matScale(x, y, z);
+	helicopter.matScale(x, y, z);
+	grotor.matScale(x, y, z);
+	protor.matScale(x, y, z);
 }
 
 void helicopterMatrixTranslate(GLfloat x, GLfloat y, GLfloat z)
 {
-  helicopter.matTranslate(x, y, z);
-  grotor.matTranslate(x, y, z);
-  protor.matTranslate(x, y, z);
+	helicopter.matTranslate(x, y, z);
+	grotor.matTranslate(x, y, z);
+	protor.matTranslate(x, y, z);
 }
 
 void helicopterMatrixRotate(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 {
-  helicopter.matRotate(angle, x, y, z);
-  grotor.matRotate(angle, x, y, z);
-  protor.matRotate(angle, x, y, z);
+	helicopter.matRotate(angle, x, y, z);
+	grotor.matRotate(angle, x, y, z);
+	grotor.matTranslate(0.025f, 0.0f, -0.5f);
+	protor.matRotate(angle, x, y, z);
+	protor.matTranslate(0.20f, 1.2f, 9.87f);
 }
 
 void helicopterRotateRotor(GLfloat angle)
 {
-  grotor.matRotate(angle, 0, 1, 0);
-  protor.matRotate(angle, 1, 0, 0);
+	grotor.matRotate(angle, 0, 1, 0);
+	protor.matRotate(angle, 1, 0, 0);
 }
 
 void helicopterDisplayShadow(engine::Light *l)
 {
-  helicopter.displayShadow(l);
-  grotor.displayShadow(l);
-  protor.displayShadow(l);
+	helicopter.displayShadow(l);
+	grotor.displayShadow(l);
+	protor.displayShadow(l);
 }
 
 void helicopterDisplay(void)
 {
-  helicopter.display();
-  grotor.display();
-  protor.display();
+	helicopter.display();
+	grotor.display();
+	protor.display();
 }
 
 void display(void)
 {
-  sun.position();
-  sun.newLoop();
-  face.displayShadow(&sun);
-  helicopterDisplayShadow(&sun);
+	sun.position();
+	sun.newLoop();
+	face.displayShadow(&sun);
+	helicopterDisplayShadow(&sun);
 
-  cam.position();
-  renderer.newLoop();
-  skybox.display();
-  face.display();
-  helicopterDisplay();
+	cam.position();
+	renderer.newLoop();
+	skybox.display();
+	face.display();
+	helicopterDisplay();
 
-  screen.display(sr, sg, sb, sa);
+	screen.display(sr, sg, sb, sa);
 }
 
 void reshape(GLuint w, GLuint h)
 {
-  cam.setPerspective(90.0f, w, h, 0.1f, 2000.0f);
+	cam.setPerspective(90.0f, w, h, 0.1f, 2000.0f);
 }
 
 void keyboard(GLubyte key, GLboolean state)
 {
-  keyState[key] = state;
-  
-  // if(keyState[MAJ]==true)
-  //   cam.setSpeed(0.05f);
-  // else
-  //   cam.setSpeed(0.25f);
-  
-  if(state)
-    switch(key)
-      {
-      case ESC:
-  	window.stop();
-  	break;
-      }
+	keyState[key] = state;
+
+	// if(keyState[MAJ]==GL_TRUE)
+	//   cam.setSpeed(0.05f);
+	// else
+	//   cam.setSpeed(0.25f);
+
+	if (state)
+		switch (key)
+	{
+		case ESC:
+			window.stop();
+			break;
+	}
 }
 
 void mouseMove(GLint xrel, GLint yrel)
 {
-  (void)xrel; (void)yrel;
-  // cam.mouseMove(xrel, yrel);
+	(void)xrel; (void)yrel;
+	// cam.mouseMove(xrel, yrel);
 }
 
 void init(void)
 {
-  // cam.setPositionCamera(0, 1, 0);
-  // cam.setSpeed(0.25f);
+	// cam.setPositionCamera(0, 1, 0);
+	// cam.setSpeed(0.25f);
 }
 
 void initGL(void)
 {
-  GLfloat vertex[]={-1000, 0, -1000,
-		    0, 0,//
-		    0, 1, 0,
-		    -1000, 0, 1000,
-		    0, 1,//
-		    0, 1, 0,
-		    1000, 0, 1000,
-		    1, 1,//
-		    0, 1, 0,
-		    1000, 0, -1000,
-		    1, 0,//
-		    0, 1, 0
-  };
-  GLuint index[]={0, 1, 2, 0, 2, 3};
-  GLfloat mat_ambient[] = {0.5f, 0.5f, 0.5f, 1.0f};
-  GLfloat mat_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-  GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-  GLfloat mat_shininess[] = {20.0f};
+	GLfloat vertex[] = { -1000, 0, -1000,
+		0, 0,//
+		0, 1, 0,
+		-1000, 0, 1000,
+		0, 1,//
+		0, 1, 0,
+		1000, 0, 1000,
+		1, 1,//
+		0, 1, 0,
+		1000, 0, -1000,
+		1, 0,//
+		0, 1, 0
+	};
+	GLuint index[] = { 0, 1, 2, 0, 2, 3 };
+	GLfloat mat_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	GLfloat mat_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_shininess[] = { 20.0f };
 
-  mainProgram = new engine::ShaderProgram;
-  shadowProgram = new engine::ShaderProgram;
-  skyboxProgram = new engine::ShaderProgram;
-  screenProgram = new engine::ShaderProgram;
-  mainProgram->loadProgram("shader/demoVert.c", "shader/demoFrag.c");
-  shadowProgram->loadProgram("shader/shadowVert.c", "shader/shadowFrag.c");
-  skyboxProgram->loadProgram("shader/skyboxVert.c", "shader/skyboxFrag.c");
-  screenProgram->loadProgram("shader/screenVert.c", "shader/screenFrag.c");
+	mainProgram = new engine::ShaderProgram;
+	shadowProgram = new engine::ShaderProgram;
+	skyboxProgram = new engine::ShaderProgram;
+	screenProgram = new engine::ShaderProgram;
+	mainProgram->loadProgram("shader/demoVert.c", "shader/demoFrag.c");
+	shadowProgram->loadProgram("shader/shadowVert.c", "shader/shadowFrag.c");
+	skyboxProgram->loadProgram("shader/skyboxVert.c", "shader/skyboxFrag.c");
+	screenProgram->loadProgram("shader/screenVert.c", "shader/screenFrag.c");
 
-  renderer.setShaderProgram(mainProgram);
-  renderer.setCamera(&cam);
-  renderer.setDirLight(&sun);
+	renderer.setShaderProgram(mainProgram);
+	renderer.setCamera(&cam);
+	renderer.setDirLight(&sun);
 
-  sun.configShadowMap(2048, 2048, shadowProgram);
-  sun.setDirection(1, -1, 0);
-  sun.setDimension(50, 50, 50);
-  sun.setAmbient(mat_ambient[0], mat_ambient[1], mat_ambient[2], mat_ambient[3]);
-  sun.setDiffuse(mat_diffuse[0], mat_diffuse[1], mat_diffuse[2], mat_diffuse[3]);
-  sun.setSpecular(mat_specular[0], mat_specular[1], mat_specular[2], mat_specular[3]);
+	sun.configShadowMap(2048, 2048, shadowProgram);
+	sun.setDirection(1, -1, 0);
+	sun.setDimension(50, 50, 50);
+	sun.setAmbient(mat_ambient[0], mat_ambient[1], mat_ambient[2], mat_ambient[3]);
+	sun.setDiffuse(mat_diffuse[0], mat_diffuse[1], mat_diffuse[2], mat_diffuse[3]);
+	sun.setSpecular(mat_specular[0], mat_specular[1], mat_specular[2], mat_specular[3]);
 
-  skybox.load("resources/Skybox/rightred2.jpg", "resources/Skybox/leftred2.jpg",
-	      "resources/Skybox/topred2.jpg", "resources/Skybox/botred2.jpg",
-	      "resources/Skybox/frontred2.jpg", "resources/Skybox/backred2.jpg",
-	      100, &cam, skyboxProgram);
-  skybox.rotate(0, 0, 0, 0);
-  
-  face.setRenderer(&renderer);
-  face.createObject(sizeof vertex, vertex,
-  		    sizeof index, index,
-  		    "resources/sand.jpg",
-		    mat_ambient, mat_diffuse, mat_specular, mat_shininess);
+	skybox.load("resources/Skybox/rightred2.jpg", "resources/Skybox/leftred2.jpg",
+		"resources/Skybox/topred2.jpg", "resources/Skybox/botred2.jpg",
+		"resources/Skybox/frontred2.jpg", "resources/Skybox/backred2.jpg",
+		100, &cam, skyboxProgram);
+	skybox.rotate(0, 0, 0, 0);
 
-  helicopter.setRenderer(&renderer);
-  helicopter.loadObj("resources/UH-60_Blackhawk/corps.obj", 1);
-  helicopter.sortObject();
+	face.setRenderer(&renderer);
+	face.createObject(sizeof vertex, vertex,
+		sizeof index, index,
+		"resources/sand.jpg",
+		mat_ambient, mat_diffuse, mat_specular, mat_shininess);
 
-  grotor.setRenderer(&renderer);
-  grotor.loadObj("resources/UH-60_Blackhawk/grotor.obj");
-  grotor.sortObject();
-  
-  protor.setRenderer(&renderer);
-  protor.loadObj("resources/UH-60_Blackhawk/protor.obj", 1);
-  protor.sortObject();
+	helicopter.setRenderer(&renderer);
+	helicopter.loadObj("resources/UH-60_Blackhawk/corps.obj", 1);
+	helicopter.sortObject();
 
-  screen.init(screenProgram);
-  sr = sg = sb = 0.0f;
-  sa = 1.0f;
+	grotor.setRenderer(&renderer);
+	grotor.loadObj("resources/UH-60_Blackhawk/grotor.obj");
+	grotor.sortObject();
 
-  // helicopterMatrixIdentity();
-  // helicopterMatrixScale(2, 2, 2);
-  // helicopterMatrixTranslate(0, 5, 0);
-  // helicopterMatrixRotate(-90, 1, 0, 0);
+	protor.setRenderer(&renderer);
+	protor.loadObj("resources/UH-60_Blackhawk/protor.obj", 1);
+	protor.sortObject();
+
+	screen.init(screenProgram);
+	sr = sg = sb = 0.0f;
+	sa = 1.0f;
+
+	// helicopterMatrixIdentity();
+	// helicopterMatrixScale(2, 2, 2);
+	// helicopterMatrixTranslate(0, 5, 0);
+	// helicopterMatrixRotate(-90, 1, 0, 0);
 }
 
 int main(int argc, char **argv)
 {
-  init();
-  
-  if(argc < 3)
-    window.initWindow("Demo", 800, 600);
-  else
-    window.initWindow("Demo", atoi(argv[1]), atoi(argv[2]));
-  window.setDisplayFunc(display);
-  window.setIdleFunc(sequence);
-  window.setReshapeFunc(reshape);
-  window.setKeyboardFunc(keyboard);
-  window.setMouseMoveFunc(mouseMove);
-  
-  initGL();
-  
-  if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
-    std::cerr << Mix_GetError() << std::endl;
-  song = Mix_LoadMUS("resources/song/song.mp3");
-  Mix_PlayMusic(song, -1);
-  Mix_VolumeMusic(MIX_MAX_VOLUME/2);
+	init();
 
-  window.mainLoop();
+	if (argc < 3)
+		window.initWindow("Demo", 800, 600);
+	else
+		window.initWindow("Demo", atoi(argv[1]), atoi(argv[2]));
+	window.setDisplayFunc(display);
+	window.setIdleFunc(sequence);
+	window.setReshapeFunc(reshape);
+	window.setKeyboardFunc(keyboard);
+	window.setMouseMoveFunc(mouseMove);
 
-  Mix_FreeMusic(song);
-  Mix_CloseAudio();
-  
-  delete mainProgram;
-  delete shadowProgram;
-  delete skyboxProgram;
-  delete screenProgram;
-  
-  return 0;
+	initGL();
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
+		std::cerr << Mix_GetError() << std::endl;
+	song = Mix_LoadMUS("resources/song/song.mp3");
+	Mix_PlayMusic(song, -1);
+	Mix_VolumeMusic(MIX_MAX_VOLUME / 2);
+
+	window.mainLoop();
+
+	Mix_FreeMusic(song);
+	Mix_CloseAudio();
+
+	delete mainProgram;
+	delete shadowProgram;
+	delete skyboxProgram;
+	delete screenProgram;
+
+	return 0;
 }
--- demo/sequence.cpp
+++ demo/sequence.cpp
@@ -2,254 +2,294 @@
 
 void sequence(void)
 {
-  GLfloat mat1[16];
-  engine::Vector3D<GLfloat> target;
-  static GLubyte step = 0;
-  static GLfloat rotor = 0, height = 3, angle, dist, a = 0.01f;
-  static GLuint timeStart = SDL_GetTicks();
-  
-  helicopterMatrixIdentity();
-  helicopterMatrixScale(2, 2, 2);
-  helicopterMatrixTranslate(0, height, 0);
-  helicopterRotateRotor(rotor);
-  
-  matrixLoadIdentity(mat1);
-  
-  switch (step)
-    {
-    case 0: // 0
-      sr = sg = sb = 0.0f;
-      if(sa > 0.0f)
-  	sa -= 0.001f;
-      
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, -20, 0, 1, 0);
-      matrixTranslate(mat1, 10, 0, 0);
-      
-      if((SDL_GetTicks() - timeStart)>17000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  sa = 1.0f;
-  	}
-      break;
-      
-    case 1: // 17000
-      sr = sg = sb = 1.0f;
-      if(sa > 0.0f)
-  	sa -= 0.05f;
-      
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixTranslate(mat1, 0, 10, -50);
-  
-      rotor += 1.0f;
-      if((SDL_GetTicks() - timeStart)>550)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  sa = 1.0f;
-  	}
-      break;
-      
-    case 2: // 17550
-      if(sa > 0.0f)
-  	sa -= 0.05f;
-      
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixTranslate(mat1, 10, 10, -20);
-  
-      rotor += 10.0f;
-      if((SDL_GetTicks() - timeStart)>550)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  sa = 1.0f;
-	  angle = 0.0f;
-  	}
-      break;
-      
-    case 3: // 18100
-      if(sa > 0.0f)
-  	sa -= 0.05f;
-      
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0, 1, 0);
-      matrixTranslate(mat1, -10, 1, -8);
-  
-      rotor += 20.0f;
-      angle += 0.01f;
-      if((SDL_GetTicks() - timeStart)>10800)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  angle=179.0f;
-  	}
-      break;
+	GLfloat mat1[16];
+	static engine::Vector3D<GLfloat> target;
+	static GLubyte step = 0;
+	static GLfloat angle, dist, a = 0, rotor = 0, height = 2.8f;
+	static GLuint timeStart = SDL_GetTicks();
 
-    case 4: // 28900      
-      target = helicopter.getPosition();
-      target._z -= 10.0f;
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 1.0, 5.0);
-      
-      rotor += 20.0f;
-      angle -= 0.1f;
-      if((SDL_GetTicks() - timeStart)>7900)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  angle = -30.0f;
-  	}
-      break;
+	helicopterMatrixIdentity();
+	helicopterMatrixScale(2, 2, 2);
+	helicopterMatrixTranslate(0, height, 0);
+	helicopterMatrixRotate(a, 0.0, 1.0, 0.0);
 
-    case 5:// 36800      
-      target = helicopter.getPosition();
-      target._z -= 10.0f;
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.5, 15.0);
-      
-      rotor += 20.0f;
-      angle -= 0.1f;
-      if((SDL_GetTicks() - timeStart)>2000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  angle = 180.0f;
-  	}
-      break;
+	matrixLoadIdentity(mat1);
 
-    case 6:// 38800
-      target = helicopter.getPosition();
-      target._z -= 10.0f;
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.0, 15.0);
-      
-      rotor += 20.0f;
-      angle += 0.2f;
-      if((SDL_GetTicks() - timeStart)>2000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  angle = -10.0f;
-  	}
-      break;
+	switch (step)
+	{
+	case 0: // Debut
+		sr = sg = sb = 0.0f;
+		if (sa > 0.0f)
+			sa -= 0.001f;
 
-    case 7:// 40800
-      target = helicopter.getPosition();
-      target._z += 10.0f;
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 1.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.0, 15.0);
-      
-      rotor += 20.0f;
-      angle += 0.2f;
-      if((SDL_GetTicks() - timeStart)>2000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	  angle = 220.0f;
-	  dist = 100.0f;
-  	}
-      break;
+		target = helicopter.getPosition();
 
-    case 8:// 42800      
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.0, dist);
-      
-      rotor += 20.0f;
-      dist -= 0.6f;
-      if((SDL_GetTicks() - timeStart)>2100)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	}
-      break;
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, -20, 0, 1, 0);
+		matrixTranslate(mat1, 10, 0, 0);
 
-    case 9:// 44900
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.0f - height, dist);
-      
-      rotor += 20.0f;
-      height += a;
-      if(a<0.5)
-	a += 0.01f;
-      if((SDL_GetTicks() - timeStart)>8100)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	}
-      break;
+		if ((SDL_GetTicks() - timeStart) > 15700) // PAF 1
+		{
+			step++;
+			sa = 1.0f;
+		}
+		break;
 
-    case 10:// 53000
-      target = helicopter.getPosition();
-      
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 0.0, 1.0, 15.0);
-      
-      rotor += 20.0f;
-      height += 0.1f;
-      angle += 0.5f;
-      if((SDL_GetTicks() - timeStart)>8000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-  	}
-      break;
+	case 1:
+		sr = sg = sb = 1.0f;
+		if (sa > 0.0f)
+			sa -= 0.05f;
 
-    case 11:// 61000
-      target = helicopter.getPosition();
+		target = helicopter.getPosition();
 
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
-      matrixTranslate(mat1, 1.0, 10.0, 0.0);
-      
-      rotor += 20.0f;
-      angle += 0.5f;
-      if((SDL_GetTicks() - timeStart)>5000)
-  	{
-  	  step++;
-  	  timeStart = SDL_GetTicks();
-	  angle = 200.0f;
-  	}
-      break;
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixTranslate(mat1, 0, 10, -50);
 
-    case 12:
-      target = helicopter.getPosition();
-      target._z -= 10.0f;
+		rotor += 1.0f;
+		if ((SDL_GetTicks() - timeStart) > 16450) // PAF 2
+		{
+			step++;
+			sa = 1.0f;
+		}
+		break;
 
-      matrixTranslate(mat1, target._x, target._y, target._z);
-      matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
-      matrixTranslate(mat1, 0.0, 0.0, 20.0);
-      
-      rotor += 20.0f;
-      angle -= 0.01f;
-    }
-  
-  sun.setPosition(helicopter.getPosition()._x, helicopter.getPosition()._y, helicopter.getPosition()._z);
-  cam.setPositionCamera(mat1[12], mat1[13], mat1[14]);
-  cam.setPositionTarget(target._x, target._y, target._z);
-  
-  // cam.keyboardMove(keyState[26], keyState[22], keyState[4], keyState[7]);
+	case 2:
+		if (sa > 0.0f)
+			sa -= 0.05f;
+
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixTranslate(mat1, 10, 10, -20);
+
+		rotor += 10.0f;
+		if ((SDL_GetTicks() - timeStart) > 17200) // PAF 3
+		{
+			step++;
+			sa = 1.0f;
+			angle = 0.0f;
+		}
+		break;
+
+	case 3:
+		if (sa > 0.0f)
+			sa -= 0.05f;
+
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0, 1, 0);
+		matrixTranslate(mat1, -10, 1, -8);
+
+		rotor += 20.0f;
+		angle += 0.01f;
+		if ((SDL_GetTicks() - timeStart) > 27700) // Front
+		{
+			step++;
+			angle = 179.0f;
+		}
+		break;
+
+	case 4:
+		target = helicopter.getPosition();
+		target._z -= 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 1.0, 5.0);
+
+		rotor += 20.0f;
+		angle -= 0.1f;
+		if ((SDL_GetTicks() - timeStart) > 35700) // Cote
+		{
+			step++;
+			angle = -30.0f;
+		}
+		break;
+
+	case 5:
+		target = helicopter.getPosition();
+		target._z -= 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.5, 15.0);
+
+		rotor += 20.0f;
+		angle -= 0.1f;
+		if ((SDL_GetTicks() - timeStart) > 37700) // Front
+		{
+			step++;
+			angle = 180.0f;
+		}
+		break;
+
+	case 6:
+		target = helicopter.getPosition();
+		target._z -= 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, 15.0);
+
+		rotor += 20.0f;
+		angle += 0.2f;
+		if ((SDL_GetTicks() - timeStart) > 39700) // Arriere
+		{
+			step++;
+			angle = -10.0f;
+		}
+		break;
+
+	case 7:
+		target = helicopter.getPosition();
+		target._z += 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 1.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, 15.0);
+
+		rotor += 20.0f;
+		angle += 0.2f;
+		if ((SDL_GetTicks() - timeStart) > 41700) // Avancer
+		{
+			step++;
+			angle = 220.0f;
+			dist = 100.0f;
+		}
+		break;
+
+	case 8:
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, dist);
+
+		rotor += 20.0f;
+		dist -= 0.6f;
+		if ((SDL_GetTicks() - timeStart) > 43700) // Decollage
+		{
+			step++;
+			a = 0.01f;
+		}
+		break;
+
+	case 9:
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0f - height, dist);
+
+		rotor += 20.0f;
+		height += a;
+		if (a < 0.5)
+			a += 0.01f;
+		if ((SDL_GetTicks() - timeStart) > 51700) // Remise a niveau
+		{
+			step++;
+		}
+		break;
+
+	case 10:
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 0.0, 1.0, 15.0);
+
+		rotor += 20.0f;
+		height += 0.1f;
+		angle += 0.5f;
+		if ((SDL_GetTicks() - timeStart) > 59700) // Vue du dessus
+		{
+			step++;
+		}
+		break;
+
+	case 11:
+		target = helicopter.getPosition();
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 0.0, 1.0, 0.0);
+		matrixTranslate(mat1, 1.0, 10.0, 0.0);
+
+		rotor += 20.0f;
+		angle += 0.5f;
+		if ((SDL_GetTicks() - timeStart) > 67700) // Arrivee cubes
+		{
+			step++;
+			angle = 200.0f;
+		}
+		break;
+
+	case 12:
+		target = helicopter.getPosition();
+		target._z -= 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, 20.0);
+
+		rotor += 20.0f;
+		angle -= 0.01f;
+		if ((SDL_GetTicks() - timeStart) > 75700) // Helico qui tourne
+		{
+			step++;
+			angle = 200.0f;
+		}
+		break;
+
+	case 13:
+		target = helicopter.getPosition();
+		target._z -= 10.0f;
+
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, 20.0);
+
+		rotor += 20.0f;
+		angle -= 0.01f;
+		a -= 0.1f;
+		if ((SDL_GetTicks() - timeStart) > 103000) // Helico qui tombe
+		{
+			step++;
+		}
+		break;
+
+	case 14:
+		matrixTranslate(mat1, target._x, target._y, target._z);
+		matrixRotate(mat1, angle, 1.0, 0.0, 0.0);
+		matrixTranslate(mat1, 0.0, 0.0, 20.0);
+
+		rotor += 20.0f;
+		angle -= 0.01f;
+		a -= 0.1f;
+		height -= 0.5f;
+		if ((SDL_GetTicks() - timeStart) > 106500) // Fin
+		{
+			step++;
+			sr = sg = sb = 0.0f;
+			sa = 1.0f;
+		}
+		break;
+
+	case 15:
+		if ((SDL_GetTicks() - timeStart) > 149000) // Fin
+			step++;
+		break;
+
+	case 16:
+		window.stop();
+		break;
+	}
+
+	helicopterRotateRotor(rotor);
+
+	sun.setPosition(helicopter.getPosition()._x, helicopter.getPosition()._y, helicopter.getPosition()._z);
+	cam.setPositionCamera(mat1[12], mat1[13], mat1[14]);
+	cam.setPositionTarget(target._x, target._y, target._z);
+
+	// cam.keyboardMove(keyState[26], keyState[22], keyState[4], keyState[7]);
 }
