13685734a08c1e43cd99397c8ce944adc8a3348e
ili1042 <ili1042@shubunkin.bocal.cs.univ-paris8.fr>
2014-05-28 16:58:55 +0200

    Cull Face Activate

--- engine/SkyBox.cpp
+++ engine/SkyBox.cpp
@@ -91,10 +91,10 @@ void engine::SkyBox::load(std::string posx, std::string negx,
 	};
 	GLuint indexArray[] = {
 		0, 1, 2, 0, 2, 3,
-		4, 5, 6, 4, 6, 7,
-		0, 1, 5, 0, 5, 4,
+		4, 7, 6, 4, 6, 5,
+		0, 4, 5, 0, 5, 1,
 		3, 2, 6, 3, 6, 7,
-		1, 2, 6, 1, 6, 5,
+		1, 5, 6, 1, 6, 2,
 		0, 3, 7, 0, 7, 4
 	};
 	_numElement = (GLuint)sizeof(indexArray) / (GLuint)sizeof(GLuint);
